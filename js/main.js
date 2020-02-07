a6c71c26e6ee267ef08216d5bddd437b223ac768
黄斌 <huangbin@huangbindeMac-mini.local>
2020-02-07 16:35:00 +0800

    video

--- js/main.js
+++ js/main.js
@@ -3,6 +3,9 @@ $(document).ready(function(){
     //document.getElementById("videoID").src = videourl+$(this).attr("data-id");
 		document.getElementById("videoID").play();
     $('.playid').css("display","flex");
-    TweenMax.from($('.videotext'),1,{y:100,repeat:-1,yoyo:true});
+    //TweenMax.from($('.videotext'),1,{y:300,repeat:-1,yoyo:true});
+    TweenMax.from($('.videotext img'),1.5,{rotationY:360,repeat:-1,yoyo:true,onComplete:function(){
+
+    }});
   });
 });
